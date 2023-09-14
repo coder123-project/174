23dc5a9cbf71c52af39116bbd7811a7c0c010468
coder123-project <83872425+coder123-project@users.noreply.github.com>
2023-09-14 23:22:46 +0530

    Add files via upload

---
+++ js/createModel.js
@@ -0,0 +1,81 @@
+AFRAME.registerComponent("createmodels", {
+  init: async function() {
+    var models = await this.getModels();
+    var barcodes = Object.keys(models);
+
+    //add the code to map according to barcode
+ 
+      var model = models[barcode];
+      this.createModel(model);
+ 
+  },
+  getModels: function() {
+    return fetch("js/models.json")
+      .then(res => res.json())
+      .then(data => data);
+  },
+  createModel: function(model) {
+    var barcodeValue = model.barcode_value;
+    var modelUrl = model.model_url;
+    var modelName = model.model_name;
+
+    //var scene = document.querySelector("a-scene");
+    
+    //var scene = document.querySelector("a-entity");
+
+    
+    //var scene = document.createElement("a-scene");
+
+    
+    //var scene = document.querySelector("a-amrker");
+
+
+
+
+
+
+
+
+
+
+
+    //var marker = document.createElement("a-scene");
+    
+    //var marker = document.documentQuery("a-entity");
+    
+    //var marker = document.create("a-marker");
+    
+    //var marker = document.createElement("a-marker");
+
+    marker.setAttribute("id", `marker-${modelName}`);
+    marker.setAttribute("type", "barcode");
+    marker.setAttribute("model_name", modelName);
+    marker.setAttribute("value", barcodeValue);
+    marker.setAttribute("markerhandler", {});
+    scene.appendChild(marker);
+
+    if (barcodeValue === 0) {
+      var modelEl = document.createElement("a-entity");
+      modelEl.setAttribute("id", `${modelName}`);
+      modelEl.setAttribute("geometry", {
+        primitive: "box",
+        width: model.width,
+        height: model.height
+      });
+      modelEl.setAttribute("position", model.position);
+      modelEl.setAttribute("rotation", model.rotation);
+      modelEl.setAttribute("material", {
+        color: model.color
+      });
+      marker.appendChild(modelEl);
+    } else {
+      var modelEl = document.createElement("a-entity");
+      modelEl.setAttribute("id", `${modelName}`);
+      modelEl.setAttribute("gltf-model", `url(${modelUrl})`);
+      modelEl.setAttribute("scale", model.scale);
+      modelEl.setAttribute("position", model.position);
+      modelEl.setAttribute("rotation", model.rotation);
+      marker.appendChild(modelEl);
+    }
+  }
+});
